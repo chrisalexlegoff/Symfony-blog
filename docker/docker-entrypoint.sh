9903463b267a48b712c1658f0ab4590fded3e8b6
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2024-02-16 15:09:20 +0100

    test

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,10 +4,10 @@ if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ]; then
     if [ ! -e /var/www/html/public/index.php ]; then
         if [ -n "$SYMFONY_PARAMS" ]; then
             echo "CREATING PROJECT with params: /usr/bin/symfony new /var/www/html $SYMFONY_PARAMS"
-            /usr/bin/symfony new /var/www/html "$SYMFONY_PARAMS"
+            cd /var/www/html && composer create-project "symfony/skeleton $SYMFONY_PARAMS"
         else
             echo "CREATING PROJECT with std-params: /usr/bin/symfony new /var/www/html $SYMFONY_PARAMS_STD"
-            /usr/bin/symfony new /var/www/html "$SYMFONY_PARAMS_STD"
+            cd /var/www/html && composer create-project "symfony/skeleton $SYMFONY_PARAMS_STD"
         fi
     else
         if [ ! -e /var/www/html/vendor ]; then
