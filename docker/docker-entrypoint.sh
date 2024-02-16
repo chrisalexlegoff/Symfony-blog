7c4b49e540295d6e7a2119827a53577db6effe15
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2024-02-16 13:43:19 +0100

    test

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ "$1" == apache2* ]] || [ "$1" = 'php-fpm' ]; then
+    if [ ! -e /var/www/html/public/index.php ]; then
+        if [ -n "$SYMFONY_PARAMS" ]; then
+            echo "CREATING PROJECT with params: /usr/bin/symfony new /var/www/html $SYMFONY_PARAMS"
+            /usr/bin/symfony new /var/www/html "$SYMFONY_PARAMS"
+        else
+            echo "CREATING PROJECT with std-params: /usr/bin/symfony new /var/www/html $SYMFONY_PARAMS_STD"
+            /usr/bin/symfony new /var/www/html "$SYMFONY_PARAMS_STD"
+        fi
+    else
+        if [ ! -e /var/www/html/vendor ]; then
+            echo "INSTALLING DEPENDENCIES with composer install"
+            cd /var/www/html && composer install
+        fi
+    fi
+fi
+
+echo "STARTING APACHE"
+
+exec "$@"
