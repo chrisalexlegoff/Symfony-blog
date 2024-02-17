d8193f1531f175e43ca23d4d2870bc15ace5a76d
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2024-02-17 16:57:58 +0100

    Home controller

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
-    composer install --prefer-dist --no-progress --no-interaction && symfony serve
+    XDEBUG_MODE=off composer install --prefer-dist --no-progress --no-interaction && symfony serve
+else
+    symfony serve
 fi
